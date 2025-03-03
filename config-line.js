8ac920a378bdd3219f06039f4c5349e6b0eb801c
amelia-bodin <147128939+amelia-bodin@users.noreply.github.com>
2025-03-03 14:46:17 +0100

    Add files via upload

---
+++ config-line.js
@@ -0,0 +1,261 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1740998593331,
+  "assetsPath": "C:\\Users\\amelia.bodin\\AppData\\Local\\Programs\\Gapminder Offline\\resources\\preview-data\\",
+  "ddfPath": "./data/ddf_lan",
+  "lastModified": 1740998593331,
+  "path": "./data/ddf_lan",
+  "reader": "ddf"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "geo",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "geo",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "time"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "time"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "time",
+    "syncModels": [],
+    "use": "indicator",
+    "which": "year",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log"
+     ]
+    },
+    "autoconfig": {
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "syncModels": [],
+    "use": "indicator",
+    "which": "ant_kon",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "_default": "#ffb600"
+    },
+    "paletteHiddenKeys": [],
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "constant",
+    "which": "_default"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "syncModels": [],
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "scaleType": "ordinal",
+    "syncModels": [],
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 150,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "year",
+   "end": "2023",
+   "endBeforeForecast": "2024",
+   "endOrigin": null,
+   "endSelected": "2023",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "pauseBeforeForecast": true,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "showForecast": true,
+   "start": "2009",
+   "startOrigin": null,
+   "startSelected": "2009",
+   "step": 1,
+   "unit": "year",
+   "value": "2023"
+  }
+ },
+ "ui": {
+  "buttons": [
+   "colors",
+   "find",
+   "moreoptions",
+   "presentation",
+   "sidebarcollapse",
+   "fullscreen"
+  ],
+  "chart": {
+   "curve": "curveMonotoneX",
+   "labels": {
+    "min_number_of_entities_when_values_hide": 2
+   },
+   "showForecastOverlay": false,
+   "whenHovering": {
+    "hideVerticalNow": false,
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true,
+    "showTooltip": false
+   }
+  },
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false,
+     "panelMode": "show"
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "colors",
+    "presentation",
+    "technical",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find"
+   ]
+  },
+  "presentation": false,
+  "sidebarCollapse": false,
+  "splash": false
+ },
+ "chartType": "LineChart"
+};
